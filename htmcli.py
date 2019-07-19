ae2e441f2c78aca066dca4bd61c18d9ba6b29f95
tf-msdev <v-fita@microsoft.com>
2019-07-19 18:01:05 +0200

    Implemented project creation. Updating in progress

--- htmcli.py
+++ htmcli.py
@@ -4,6 +4,7 @@ import datetime
 from htm_msopenmaps_custom_api import UserLoginAPI
 from swaggerAPIClient.swagger_client.api import project_admin_api, mapping_api, grid_api
 import re
+import json
 
 parser = argparse.ArgumentParser(description='HOT Tasking Manager command line interface')
 parser.add_argument('api', type=str, nargs=1)
@@ -14,6 +15,33 @@ parser.add_argument('-p', '--password', type=str)
 parser.add_argument('-a', '--areaOfInterest', type=str)
 parser.add_argument('-n', '--projectName', type=str)
 parser.add_argument('-t', '--tasks', type=str)
+
+parser.add_argument('-i', '--projectId', type=str)
+parser.add_argument('--allowedUsernames', type=str)
+parser.add_argument('--campaignTag', type=str)
+parser.add_argument('--changesetComment', type=str)
+parser.add_argument('--defaultLocale', type=str)
+parser.add_argument('--dueDate', type=str)
+parser.add_argument('--enforceMapperLevel', type=str)
+parser.add_argument('--enforceValidatorRole', type=str)
+parser.add_argument('--entitiesToMap', type=str)
+parser.add_argument('--imagery', type=str)
+parser.add_argument('--josmPreset', type=str)
+parser.add_argument('--licenseId', type=str)
+parser.add_argument('--mapperLevel', type=str)
+parser.add_argument('--mappingTypes', type=str)
+parser.add_argument('--organisationTag', type=str)
+parser.add_argument('--priorityAreas', type=str)
+parser.add_argument('--private', type=str)
+parser.add_argument('--description', type=str)
+parser.add_argument('--instructions', type=str)
+parser.add_argument('--locale', type=str)
+#parser.add_argument('-n', '--name', type=str)
+parser.add_argument('--perTaskInstructions', type=str)
+parser.add_argument('--shortDescription', type=str)
+parser.add_argument('--projectPriority', type=str)
+parser.add_argument('--projectStatus', type=str)
+parser.add_argument('--taskCreationMode', type=str)
 
 args = parser.parse_args()
 print(args) #DEBUG_ONLY
@@ -60,16 +88,23 @@ elif(args.api[0] == "project"):
             login_api_instance = pickle.load(store)
         except:
             print("Authentication needed")
+            exit(2)
         finally:
             if(store != None):
                 store.close()
         
-        if((datetime.datetime.now() - login_api_instance.timeCreated).total_seconds() / 60. > 10.):
-            ("Authentication needed")
+        if(int((datetime.datetime.now() - login_api_instance.timeCreated).total_seconds() / 60.) > 30):
+            print("Reauthentication needed")
+            exit(2)
         
+        body = {
+          "arbitraryTasks" : False,
+          # to fill
+        }
+
         pattern_1 = r"(\[.*\])"
         pattern_2 = r"\[\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\]"
-        matches_1 = re.findall(pattern_1, "[ [1.5, 2.33], [22.12, 3], [3, 4], [4, 5] ]")
+        matches_1 = re.findall(pattern_1, args.areaOfInterest)
         aoi_features = []
         for match_1 in matches_1:
             match_1 = match_1[1:-1]
@@ -80,9 +115,144 @@ elif(args.api[0] == "project"):
                 y = float(match_2[1])
                 point = [x, y]
                 polygon.append(point)
-            
-            
+            newFeature = {"geometry": {
+                "type": "MultiPolygon"
+                },
+                "properties": {
+                    "isSquare": True,
+                    "x": 2400,
+                    "y": 1750,
+                    "zoom": 15
+                },
+                "type": "Feature"
+            }
+            newFeature["geometry"]["coordinates"] = [[polygon]]
+            aoi_features.append(newFeature)
+        aoi = {}
+        aoi["type"] = "FeatureCollection"
+        aoi["features"] = aoi_features
+        #print(aoi)
 
+        pattern_1 = r"(\[.*\])"
+        pattern_2 = r"\[\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\]"
+        matches_1 = re.findall(pattern_1, args.areaOfInterest)
+        tasks_features = []
+        for match_1 in matches_1:
+            match_1 = match_1[1:-1]
+            matches_2 = re.findall(pattern_2, match_1)
+            polygon = []
+            for match_2 in matches_2:
+                x = float(match_2[0])
+                y = float(match_2[1])
+                point = [x, y]
+                polygon.append(point)
+            newFeature = {"geometry": {
+                "type": "MultiPolygon"
+                },
+                "properties": {
+                    "isSquare": True,
+                    "x": 2400,
+                    "y": 1750,
+                    "zoom": 15
+                },
+                "type": "Feature"
+            }
+            newFeature["geometry"]["coordinates"] = [[polygon]]
+            tasks_features.append(newFeature)
+        tasks = {}
+        tasks["type"] = "FeatureCollection"
+        tasks["features"] = tasks_features
+        #print(tasks)
 
+        body["projectName"] = args.projectName
+        body["areaOfInterest"] = aoi
+        body["tasks"] = tasks
+        print(body)
 
+        reply = project_admin_api.ProjectAdminApi().api_v1_admin_project_put(body, login_api_instance.get_authorization(), _preload_content=False)
+        print(reply.data.decode())
+
+    elif(args.operation[0] == "update"):
+
+        if(args.projectId == None):
+            print("usage: htmcli.py project update projectId ...[-a AREAOFINTEREST] [-n PROJECTNAME] [-t TASKS]")
+            exit(1)
+
+        store = None
+        login_api_instance = None
+        try:
+            store = open('authentication_info.obj', 'rb')
+            login_api_instance = pickle.load(store)
+        except:
+            print("Authentication needed")
+            exit(2)
+        finally:
+            if(store != None):
+                store.close()
         
+        if(int((datetime.datetime.now() - login_api_instance.timeCreated).total_seconds() / 60.) > 30):
+            print("Reauthentication needed")
+            exit(2)
+
+        reply = project_admin_api.ProjectAdminApi().api_v1_admin_project_project_id_get(login_api_instance.get_authorization(), int(args.projectId), _preload_content=False)
+        reply = json.loads(reply.data.decode())
+        #print(reply)
+
+        print(args._get_kwargs())
+
+        for arg_pair in args._get_kwargs():
+            arg_name = arg_pair[0]
+            arg_val = arg_pair[1]
+
+            if(arg_val == None or arg_name == "api" or arg_name == "operation"):
+                reply.pop(arg_name, None)
+                continue
+
+            if(arg_name == "allowedUsernames"):
+                arg_val = arg_val.strip('[')
+                arg_val = arg_val.strip(']')
+                u_list = []
+                for u in arg_val.split(","):
+                    u_list.append(u)
+                reply["allowedUsernames"] = u_list
+            
+            elif(arg_name == "enforceMapperLevel"):
+                if(arg_val == "true" or arg_val == "True"):
+                    reply["enforceMapperLevel"] = True
+                else:
+                    reply["enforceMapperLevel"] = False
+
+            elif(arg_name == "enforceValidatorRole"):
+                if(arg_val == "true" or arg_val == "True"):
+                    reply["enforceValidatorRole"] = True
+                else:
+                    reply["enforceValidatorRole"] = False
+
+            elif(arg_name == "licenseId"):
+                reply["licenseId"] = int(arg_val)
+
+            elif(arg_name == "mappingTypes"):
+                pattern = r"[^A-Za-z]*([A-Za-z]+)[^A-Za-z]*"
+                matches = re.findall(pattern, arg_val)
+                mt_list = []
+
+                for match in matches:
+                    mt_list.append(match)
+
+                reply["mappingTypes"] = mt_list
+
+            elif(arg_name == "description" or arg_name == "instructions" or arg_name == "projectName" or arg_name == "locale" or arg_name == "perTaskInstructions" or arg_name == "shortDescription"):
+                if(arg_name == "projectName"):
+                    reply["projectInfoLocales"]["name"] = arg_val
+                else:
+                    reply["projectInfoLocales"][arg_name] = arg_val
+
+            else:
+                reply[arg_name] = arg_val
+
+        print(reply)
+        reply = project_admin_api.ProjectAdminApi().api_v1_admin_project_project_id_post(reply, login_api_instance.get_authorization(), int(args.projectId), _preload_content=False)
+        print(reply.data.decode())
+
+    else:
+        print("Project api operations are: create, update")
--- test2.py
+++ test2.py
@@ -1,9 +1,28 @@
 from htm_msopenmaps_custom_api import UserLoginAPI
-from swaggerAPIClient.swagger_client.api import project_admin_api
+from swaggerAPIClient.swagger_client.api import project_admin_api, grid_api
 import json
 import re
+from shapely.geometry import Point,Polygon,MultiPolygon
 
-body = {
+""" my_api_instance = UserLoginAPI()
+my_api_instance.obtain_authorization("v-fita@microsoft.com", "Vvardenfello1")
+project_admin_api_instance = project_admin_api.ProjectAdminApi()
+
+body["tasks"] = MyAPI.util_split_quad_multipolygon_tasks(body["tasks"])
+print(body["tasks"])
+encoded_body = json.dumps(body)
+reply = project_admin_api_instance.api_v1_admin_project_put(body, my_api_instance.get_authorization(), _preload_content=False)
+print(reply.data.decode()) """
+""" pattern_1 = r"(\[.*\])"
+pattern_2 = r"\[\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\]"
+matches_1 = re.findall(pattern_1, "[ [1.5, 2.33], [22.12, 3], [3, 4], [4, 5] ]")
+for match_1 in matches_1:
+    match_1 = match_1[1:-1]
+    matches_2 = re.findall(pattern_2, match_1)
+    for match_2 in matches_2:
+      print(match_2[0], match_2[1]) """
+
+""" body = {
   "arbitraryTasks" : False,
   "areaOfInterest" : {
     "features": [
@@ -12,7 +31,7 @@ body = {
           "type": "MultiPolygon",
           "coordinates": [
             [
-              [ [21., 45.], [21., 44.7], [20., 44.7], [20., 45.], [21., 45.] ]
+              [ [21, 45], [21, 44.7], [20, 44.7], [20, 45], [21, 45] ]
             ]
           ]
         },
@@ -35,15 +54,15 @@ body = {
           "type": "MultiPolygon",
           "coordinates": [
               [
-                [ [21., 45.], [21., 44.7], [20., 44.7], [20., 45.], [21., 45.] ]
+                [ [21, 45.3], [21, 44.5], [20, 44.7], [20, 45], [21, 45.3] ]
               ]
             ]
         },
         "properties": {
 
           "isSquare": True,
-          "x": 57000,
-          "y": 70000,
+          "x": 2400,
+          "y": 1750,
           "zoom": 15
         },
         "type": "Feature"
@@ -53,22 +72,39 @@ body = {
   }
 }
 
-""" my_api_instance = UserLoginAPI()
+p1 = Polygon([ (21.402567, 44.818087), (21.414927, 44.817600), (21.413725, 44.825027), (21.603254, 44.823444), (21.402567, 44.818087) ])
+p1.is_valid
+
+my_api_instance = UserLoginAPI()
 my_api_instance.obtain_authorization("v-fita@microsoft.com", "Vvardenfello1")
+grid_api_instance = grid_api.GridApi()
 project_admin_api_instance = project_admin_api.ProjectAdminApi()
 
-body["tasks"] = MyAPI.util_split_quad_multipolygon_tasks(body["tasks"])
-print(body["tasks"])
-encoded_body = json.dumps(body)
+body_grid = body.copy()
+body_grid.pop("projectName")
+body_grid.pop("arbitraryTasks")
+body_grid["grid"] = body_grid["areaOfInterest"].copy()
+body_grid["clipToAoi"] = False
+body_grid.pop("tasks")
+body_grid["grid"]["features"][0]["properties"]["zoom"] = 19
+
+
+reply = grid_api_instance.api_v1_grid_intersecting_tiles_put(body_grid, my_api_instance.get_authorization(), _preload_content=False)
+print(reply.data.decode())
+newTasks = json.loads(reply.data.decode())
+
+body["tasks"] = newTasks
 reply = project_admin_api_instance.api_v1_admin_project_put(body, my_api_instance.get_authorization(), _preload_content=False)
-print(reply.data.decode()) """
-pattern_1 = r"(\[.*\])"
-pattern_2 = r"\[\s*([0-9\.]+)\s*,\s*([0-9\.]+)\s*\]"
-matches_1 = re.findall(pattern_1, "[ [1.5, 2.33], [22.12, 3], [3, 4], [4, 5] ]")
-for match_1 in matches_1:
-    match_1 = match_1[1:-1]
-    matches_2 = re.findall(pattern_2, match_1)
-    for match_2 in matches_2:
-      print(match_2[0], match_2[1])
-    
-      
+print(reply.data.decode())
+"""
+
+import re
+
+pattern = r"^\s*\[(.*)\]\s*$"
+matches = re.findall(pattern, "[BUILDINGS , ROADS]    ")
+print(matches)
+pattern = pattern.strip('[')
+pattern = pattern.strip(']')
+pattern = r"[^A-Za-z]*([A-Za-z]+)[^A-Za-z]*"
+matches = re.findall(pattern, matches[0])
+print(matches)
